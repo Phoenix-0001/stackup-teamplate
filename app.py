bfbad8caeae43ab8597ef140a496efbea32b76dd
Phoenix-0001 <149677560+Phoenix-0001@users.noreply.github.com>
2023-11-11 18:03:10 +0530

    Database

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from wtforms.validators import InputRequired, Length, ValidationError, Optional
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///https://drive.google.com/file/d/1KIMFPhZGMOYg0kMfrAv0__XBb3yjAGjU/view?usp=sharing'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///https://drive.google.com/file/d/1KIMFPhZGMOYg0kMfrAv0__XBb3yjAGjU/view?usp=sharing'
 app.config['SECRET_KEY'] = 'itbytes'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -182,6 +182,6 @@ def register():
     return render_template('register.html', form=form)
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-'''
+
